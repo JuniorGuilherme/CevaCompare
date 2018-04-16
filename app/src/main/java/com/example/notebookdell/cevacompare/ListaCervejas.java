43fe3d4394474d6b399b131dfa2ee70ea7e3647f
JuniorGuilherme <guilhermestads@ulbra.edu.br>
2018-04-16 10:28:47 -0300

    Tela de listagem de cervejas.

--- app/src/main/java/com/example/notebookdell/cevacompare/ListaCervejas.java
+++ app/src/main/java/com/example/notebookdell/cevacompare/ListaCervejas.java
@@ -2,12 +2,17 @@ package com.example.notebookdell.cevacompare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ListaCervejas extends AppCompatActivity {
-
+    ListView lvCervejas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_cervejas);
+
+        Cerveja c = new Cerveja();
+        lvCervejas = findViewById(R.id.lvCervejas);
+        lvCervejas.setAdapter(new AdapterCerveja(c.retornaArrayCervejas(), this));
     }
 }
